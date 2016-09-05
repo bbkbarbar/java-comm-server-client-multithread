80f9df6ecea93735b20e0493d2b7fcc3d369cbc8
Barbár <bbk.barbar@gmail.com>
2016-09-05 11:16:09 +0200

    Add Msg.Type: RESPONSE_IO_STATE

--- comm.util/src/main/java/hu/barbar/comm/util/Msg.java
+++ comm.util/src/main/java/hu/barbar/comm/util/Msg.java
@@ -22,7 +22,8 @@ public class Msg implements Serializable{
 								PWM_OUTPUT_STATES = 6,
 								RESPONSE_WORKER_INFO = 7,
 								RESPONSE_CPU_TEMP = 8,
-								RGB_COMMAND2 = 9;
+								RGB_COMMAND2 = 9,
+								RESPONSE_IO_STATE = 10;
 		
 		public static String getTypeText(int type){
 			switch (type) {
